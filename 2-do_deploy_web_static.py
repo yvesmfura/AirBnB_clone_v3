3ec14e147e6572daf3e1a488eb480a9984ea5e72
Yves IRAKOZE MFURA <132387277+yvesmfura@users.noreply.github.com>
2024-04-29 09:46:03 +0200

    filename typo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,6 @@ from fabric import api
 from fabric.contrib import files
 import os
 
-
 api.env.hosts = ['142.44.167.235', '144.217.246.199']
 api.env.user = 'ubuntu'
 api.env.key_filename = '~/.ssh/holberton'
